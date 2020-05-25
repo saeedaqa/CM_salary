fb4f7d68c1efb6b4399e2bc8ffb930096023934a
saeedaqa <47068493+saeedaqa@users.noreply.github.com>
2020-05-25 15:56:55 -0400

    Web scraper

---
+++ run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 14:44:38 2020
+
+@author: shaki
+"""
+
+
+import glassdoor_scraper as gs
+import pandas as pd
+path="/Users/shaki/Documents/my_projects/CM_salary/chromedriver"
+
+df=gs.get_jobs('construction manager', 15, False, path, 15)
